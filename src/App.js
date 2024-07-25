0ffcca768fe2473130289642a1343d4c32ef5ef3
rose912 <chioma.osuagwu@ymail.com>
2024-07-25 12:33:52 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Rosemary Obioma and is{" "}
-        <a
-          href="https://github.com/rose912/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Rosemary Obioma and is{" "}
+          <a
+            href="https://github.com/rose912/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
