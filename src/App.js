b126b8da2f8bd53f22bd2b0d6b33a45626f5a031
rose912 <chioma.osuagwu@ymail.com>
2024-08-27 12:58:52 -0400

    Added Weather.js and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
-function App() {
+ export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <weather />
         <footer>
           This project was coded by Rosemary Obioma and is{" "}
           <a
@@ -21,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li>Thursday 1:00</li>
+        <li>Sunny</li>
+      </ul>
+      </div>
+  )
+}
