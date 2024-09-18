fe3c1b57c936b2167672004af4fb713663225d68
rose912 <chioma.osuagwu@ymail.com>
2024-09-18 18:06:06 -0400

    Update changes

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./WeatherForcast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -23,6 +28,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
